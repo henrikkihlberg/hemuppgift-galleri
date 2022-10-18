6552ee62d11a1682023aff00aa9e8afb7469fb9e
henrikkihlberg <93766878+henrikkihlberg@users.noreply.github.com>
2022-10-18 10:49:25 +0200

    Functional fetch + render

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 console.log('HEJSAN')
+
+const setCookie = () => {
+
+}
+
+const fetchImages = () => {
+    fetch('http://localhost:3000/images')
+    .then(response => {
+        // console.log(response)
+        if (!response.ok) {
+            throw Error('ERROR')
+        }
+        return response.json()
+    })
+    .then(data => {
+        console.log(data.photos)
+        const html = data.photos.photo.map(image => {
+            return `
+                <div class="image">
+                    <p>
+                        <img src="https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg" />
+                    </p>
+                </div>
+            `
+        })
+        console.log(html)
+        document.querySelector("#image_carousel").insertAdjacentHTML("afterbegin", html)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+fetchImages()
+
+
+
+const loop = (data) => {
+    const gallery = document.createDocumentFragment()
+
+    data.map()
+}
+
+// var imageLibrary
+
+// fetch('http://localhost:3000/images')
+// .then((response) => response.json())
+// .then((data) => imageLibrary = data.photos.photo)
+// .then(() => console.log(imageLibrary))
+
+// imageLibrary.photos.photo?.[1]
+
+// let loop = imageLibrary.map((image) => {
+//     console.log(image)
+// })
