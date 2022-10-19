007486edf78faf785157714dbe5ca2f3b27c9573
henrikkihlberg <93766878+henrikkihlberg@users.noreply.github.com>
2022-10-19 15:27:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,39 +15,38 @@ buttons.forEach(button => {
 })
 
 const imageFetch = () => {
-var imageFetchData = [
+    var imageFetchData = [
         {param: 'forests', selector: '#forests_carousel', element: 'forests_carousel'},
         {param: 'rivers', selector: '#rivers_carousel', element: 'rivers_carousel'},
         {param: 'mountains', selector: '#mountains_carousel', element: 'rivers_carousel'}
     ]
-
     imageFetchData.forEach((info) => {
-        console.log(info)
-    fetch(`https://hemuppgift-api-wopwb5vhiq-lz.a.run.app/${info.param}`)
-    .then(response => {
-        // console.log(response)
-        if (!response.ok) {
-            throw Error('ERROR')
-        }
-        return response.json()
-    })
-    .then(data => {
-        console.log(data.photos)
-        data.photos.photo.forEach(image => {
-            document.querySelector(info.selector).insertAdjacentHTML("afterbegin",
-            `
-                <li class="slide">
-                    <img src="https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg" alt="Image" />
-                </li>
-            `
-            )
-        }) 
-    })
-    .catch(error => {
-        console.log(error)
-        let randomPictureError = `<div>Failed to fetch pictures. Error : ${error}</div>`
-        document.getElementById(info.element).innerHTML = randomPictureError
-    })
+        // console.log(info)
+        fetch(`https://hemuppgift-api-wopwb5vhiq-lz.a.run.app/${info.param}`)
+        .then(response => {
+            // console.log(response)
+            if (!response.ok) {
+                throw Error('ERROR')
+            }
+            return response.json()
+        })
+        .then(data => {
+            // console.log(data.photos)
+            data.photos.photo.forEach(image => {
+                document.querySelector(info.selector).insertAdjacentHTML("afterbegin",
+                `
+                    <li class="slide">
+                        <img src="https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg" alt="Image" />
+                    </li>
+                `
+                )
+            }) 
+        })
+        .catch(error => {
+            console.log(error)
+            let randomPictureError = `<div>Failed to fetch pictures. Error : ${error}</div>`
+            document.getElementById(info.element).innerHTML = randomPictureError
+        })
     })
 }
 imageFetch()
@@ -67,10 +66,11 @@ const fetchNature = () => {
         const pictures = data.photos.photo
         // console.log(pictures)
         const randomPicture = Math.floor(Math.random() * pictures.length)
-        const randomPictureCode = `
-        <img src="https://live.staticflickr.com/${pictures[randomPicture].server}/${pictures[randomPicture].id}_${pictures[randomPicture].secret}.jpg" alt="Random Image" class="random-picture-image"/>
-        <div class="random-picture-description">${pictures[randomPicture].title}</div>
-    `
+        const randomPictureCode =
+        `
+            <img src="https://live.staticflickr.com/${pictures[randomPicture].server}/${pictures[randomPicture].id}_${pictures[randomPicture].secret}.jpg" alt="Random Image" class="random-picture-image"/>
+            <div class="random-picture-description">${pictures[randomPicture].title}</div>
+        `
         // console.log(pictures[randomPicture].id)
         document.getElementById("random-picture").innerHTML = randomPictureCode
     })
